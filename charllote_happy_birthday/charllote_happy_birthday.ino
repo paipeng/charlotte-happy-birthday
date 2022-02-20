0eed9a00e2e7af366f09cd1e6260f9a19a5e8118
Pai Peng <sipaipv6@gmail.com>
2022-02-20 09:19:45 +0800

    draw text pixels in marquee

--- charllote_happy_birthday/charllote_happy_birthday.ino
+++ charllote_happy_birthday/charllote_happy_birthday.ino
@@ -41,6 +41,16 @@ const char HEART[] = {
   
 };
 
+const char CHARLOTTE[] = {
+0,0,1,1,1,1,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,
+0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,1,1,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,
+0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,
+0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,
+0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,
+0,1,1,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,
+0,0,1,1,1,1,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0
+};
+
 SPIClass spi_(SPI0); // MUST be SPI0 for Maix series on board LCD
 Sipeed_ST7789 lcd(SCREEN_WIDTH, SCREEN_HEIGHT, spi_);
 
@@ -48,6 +58,25 @@ Sipeed_ST7789 lcd(SCREEN_WIDTH, SCREEN_HEIGHT, spi_);
 uint8_t* data;
 int depth = 2;
 
+static int mqrquee_x = 0;
+
+void drawCharlotteMarquee(int x, int y, int color) {
+  int i, j;
+  int k, l;
+  int zoom = 3;
+  lcd.fillRect(0, y, SCREEN_WIDTH, 7*zoom, COLOR_RED);  
+  for (i = 0; i < 7*zoom; i+=zoom) {
+    for (j = 0; j < 94*zoom; j+=zoom) {
+      if (CHARLOTTE[94*i/zoom+j/zoom] == 1) {
+        for (k = 0; k < zoom; k++) {
+          for (l = 0; l < zoom; l++) {
+            lcd.drawPixel(j + x + l, i + y + k, color);            
+          }
+        }
+      }
+    }
+  }
+}
 
 void drawHeart(int x, int y, int color)
 {
@@ -70,12 +99,12 @@ void func()
   
   lcd.setTextSize(3);
   lcd.setTextColor(COLOR_WHITE);
-  lcd.setCursor(30, 30);
+  lcd.setCursor(40, 30);
   lcd.println("Happy Birthday");
   
   lcd.setTextSize(5);
-  lcd.setCursor(30, 90);
-  lcd.println("Charlotte");
+  //lcd.setCursor(30, 90);
+  //lcd.println("Charlotte");
   lcd.setCursor(100, 150);
   lcd.println("Peng");
 
@@ -83,9 +112,10 @@ void func()
   lcd.setCursor(100, 210);
   lcd.println("2022-02-20");
 
-  drawHeart(30, 160, COLOR_WHITE);
+  drawHeart(30, 150, COLOR_WHITE);
   
-  drawHeart(250, 160, COLOR_WHITE);
+  drawHeart(250, 150, COLOR_WHITE);
+
 }
 
 
@@ -93,11 +123,16 @@ void setup() {
   lcd.begin(15000000, COLOR_RED);
   randomSeed(analogRead(0));
   data = (uint8_t*) malloc(SCREEN_WIDTH * SCREEN_HEIGHT * depth * sizeof(uint8_t));
+  
+  func();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  func();
-  delay(100000);
+  if (mqrquee_x == -50) {
+    mqrquee_x = SCREEN_WIDTH;
+  }
+  drawCharlotteMarquee(mqrquee_x,100,COLOR_WHITE);
+  mqrquee_x -=10;
+  delay(300);
 }
